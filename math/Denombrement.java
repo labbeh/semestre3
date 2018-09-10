c8130ddc53218216c686045875d0325a8a2b3195
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2018-09-10 17:00:38 +0200

    avancement journée

--- math/Denombrement.java
+++ math/Denombrement.java
@@ -2,31 +2,40 @@ public class Denombrement
 {
 	public static int factorielle(int n)
 	{
-		if(n == 0 ) return  1; //fonctionne aussi
-		//if(n <  0 ) return -1; // code erreur
-		if(n<2) return 1;
+		if(n == 0 ) return  1;
+		if(n <  0 ) return -1; // code erreur
 
-		//int fact = n*n-1;
 		return n*factorielle(n-1);
 	}
 
-	public static int cnkFacorielle(int n, int k)
+	public static int cnkFactorielle(int n, int k)
 	{
-		return 0;
+		if(k >  n) return 0;
+		if(k <  0) return 0;
+		if(k == 0) return 0;
+		if(n <  0) return 0;
+
+		return Denombrement.factorielle(n) / (Denombrement.factorielle(k) * (Denombrement.factorielle(n-k)));
 	}
 
 	public static int cnkPascal(int n, int k)
 	{
-		return 0;
+		if(k <  0 || n <  k) return 0;
+		if(k == 0 || n == k) return 1;
+
+		return Denombrement.cnkPascal(n-1, k-1) + Denombrement.cnkPascal(n-1, k);
 	}
 
 	public static int cnkRecursif(int n, int k)
 	{
-		return 0;
+		if(k <  0 || n < k) return 0;
+		if(k == 0		  ) return 1;
+
+		return n * Denombrement.cnkRecursif(n-1, k-1) / k;
 	}
 
 	public static void main(String[] args)
 	{
-		System.out.println(Denombrement.factorielle(5));
+		System.out.println(Denombrement.cnkFactorielle(42, 23));
 	}
 }
