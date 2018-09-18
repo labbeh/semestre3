6ff8ddd6fc46ac288dbcc11cb78c64058bb507da
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2018-09-18 18:44:13 +0200

    exo codecorrecteurdetecteur a envoyer

--- M3202C/codeCorrecteur/CodeCorrecteurDetecteur.java
+++ M3202C/codeCorrecteur/CodeCorrecteurDetecteur.java
@@ -1,13 +1,9 @@
 public abstract class CodeCorrecteurDetecteur
 {
 	/**
-	* @author: labbeh
-	* @date  : 2018-09-13
+	* @author : Labbé Hugo D2
+	* @version: 1.1, 2018-09-13
 	*/
-
-	// CONSTANTES BITS POSSIBLES
-	private static final int BIT_ZERO = 0;
-	private static final int BIT_UN	  = 1;
 
 	// ATTRIBUTS
 	private int nbBitsCode;
@@ -27,30 +23,25 @@ public abstract class CodeCorrecteurDetecteur
 
 	public MotBinaire transmettre(MotBinaire bits, int n)
 	{
-		int bitAct;
+		StringBuilder mot = new StringBuilder();
 
 		for(int cpt=0; cpt<bits.nbBits(); cpt++)
 		{
-			if(cpt % n == 0)
-			{
-				bitAct = bits.get(cpt);
-
-				if(bitAct == CodeCorrecteurDetecteur.BIT_ZERO) bits.set(cpt, CodeCorrecteurDetecteur.BIT_UN	 );
-				else 										   bits.set(cpt, CodeCorrecteurDetecteur.BIT_ZERO);
-			}
+			mot.append(bits.get(cpt));
+			if(cpt % n == 0) mot.append((int)(Math.random() * 2));
 		}
 		
-		return new MotBinaire(bits);
+		return MotBinaire.fabrique(mot.toString());
 	}
 
 	public MotBinaire desecuriser(MotBinaire bits)
 	{
-		int[] motRet = new int[this.nbBitsCode];
+		int[] motRet  = new int[this.nbBitsCode];
 		int cptTabMot = 0;
 
 		for(int cpt=0; cpt<bits.nbBits(); cpt++)
 		{
-			if(cpt % this.nbBitsCode-this.nbBitsInfo != 0)
+			if(cpt % (this.nbBitsCode-this.nbBitsInfo) != 0)
 				motRet[cptTabMot++] = bits.get(cpt);
 		}
 
