e4dceede5db5d1c3776e90d4fc1040151511fac9
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2018-09-19 14:20:00 +0200

    algo à finir

--- algo/tp2/Hanoi.java
+++ algo/tp2/Hanoi.java
@@ -1,15 +1,45 @@
 public class Hanoi
 {
+	private static int nbCoup = 0;
+
 	public static void hanoi ( int nbDisque, int tourDeb, int tourFin )
 	{
-		if(nbDisque>0) hanoi(nbDisque-1, tourDeb, 6-tourFin-tourDeb);
+		if(nbDisque>0)
+		{
+			hanoi(nbDisque-1, tourDeb, 6-tourFin-tourDeb);
+
+			System.out.println(String.format("3%d", ++nbCoup) +" : "+ tourDeb+ " --> " +tourFin);
+
+			hanoi(nbDisque-1, 6-tourFin-tourDeb, tourFin);
+		}
 		
 
-		hanoi(nbDisque-1, 6-tourFin-tourDeb, tourFin);
+		//hanoi(nbDisque-1, 6-tourFin-tourDeb, tourFin);
+	}
+
+	public static void hanoiDebug ( String sep, int nbDisque, int tourDeb, int tourFin )
+	{
+		if(nbDisque>0) System.out.println(); else System.out.println(" stop");
+
+		if(nbDisque>0)
+		{
+			hanoiDebug(sep +" |   ", nbDisque-1, tourDeb, 6-tourFin-tourDeb);
+
+			System.out.println(sep + " |   |");
+			System.out.println(sep + " |   affiche " +tourDeb + " --> " +tourFin);
+			System.out.println(sep + " |   |");
+
+			hanoiDebug( sep + " |   ", nbDisque-1, 6-tourFin-tourDeb, tourFin);
+		}
+		
+
+		//hanoi(nbDisque-1, 6-tourFin-tourDeb, tourFin);
 	}
 
 	public static void main(String[] args)
 	{
-		hanoi(3, 1, 6);
+		//System.out.println()
+		//hanoi(3,1,3);
+		hanoiDebug(" ", 3,1,3);
 	}
 }
---
+++ algo/tp3/TriUtil.java
@@ -0,0 +1,83 @@
+// A rediriger dans le dossier paquetage_class
+package utilitaire;
+
+public class TriUtil
+{
+	public static void triSelection( int[] tab )
+	{
+		int max;
+		int taille = tab.length;
+
+		for(int cpt=0; cpt<tab.length; cpt++)
+		{
+			max = TriUtil.max(tab, taille);
+			taille--;
+			TriUtil.echanger(tab, max, taille);
+		}
+	}
+
+	private static int max( int[] tab, int taille )
+	{
+		int max = 0;
+
+		for(int cpt=0; cpt<taille; cpt++)
+			if(tab[cpt] > tab[max]) max = cpt;
+
+		return max;
+	}
+
+	private static void echanger( int[] tab, int posOrg, int posDest )
+	{
+		int tmp = tab[posDest];
+
+		tab[posDest] = tab[posOrg];
+		tab[posOrg ] = tmp;
+	}
+
+   	public static void triBulle( int[] tab )
+   	{
+   		for(int cpt=0; cpt<tab.length-1; cpt++)
+   		{
+   			if(tab[cpt] < tab[cpt+1]) echanger(tab, cpt, cpt+1);
+   		}
+   	}
+
+   	public static void triInsertion( int[] tab )
+   	{
+
+   	}
+
+   	public static void triRapide( int[] tab, int deb, int fin )
+   	{
+   		int pivot;
+   		if(deb < fin)
+   		{
+   			pivot = partitionner(tab, deb, fin);
+
+   			triRapide(tab, deb, pivot-1);
+   			triRapide(tab, pivot+1, fin);
+   		}
+   	}
+
+   	private static int partitionner( int[] tab, int deb, int fin )
+   	{
+   		return 0;
+   	}
+
+   	public static void main(String[] args)
+   	{
+   		int[] tab = new int[]{9,5,2,3,5,7,8,2,1,4};
+   		triSelection(tab);
+   		//triBulle(tab);
+
+   		for(int cpt=0; cpt<tab.length; cpt++)
+   			System.out.print(tab[cpt] +" ");
+
+   		System.out.println();
+   	}
+
+   	/**
+   	* @constructor: privé et ne fait rien car classe utilitaire non instnciable
+   	*/
+	private TriUtil(){}
+}
