1a545eca4a3d980c4372b5b9f7e8e8bc5579f332
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2018-11-06 17:13:36 +0100

    tp2 simbad en cours, correction des pbs

---
+++ cpoa/workspace/tp1Simbad/src/tp2/Exemple2.java
@@ -0,0 +1,17 @@
+package tp2;
+
+import simbad.gui.Simbad;
+
+public class Exemple2
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Exemple 2");
+		Simbad frame = new Simbad(new MyEnv() ,false);
+	}
+
+}
