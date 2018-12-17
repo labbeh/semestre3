564558722f10c531155d080dd7f84b95c26de5be
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2018-12-17 16:41:02 +0100

    projet tut interpréteur pseudo-code jours 1

--- projet_tut/eclipse/pseudocode/src/pseudoCode/metier/vars/Variable.java
+++ projet_tut/eclipse/pseudocode/src/pseudoCode/metier/vars/Variable.java
@@ -1,26 +1,32 @@
 package pseudoCode.metier.vars;
-
+/**
+ * Crée une classe Variable qui permet de stocker 
+ * une variable à partir de son type et sa valeur
+ * @author Baron Clement, Cornilleau Titouan, Cadorel Loan, Labbé Hugo, Mandé Sébastien
+ * @version 1.0, 2018-12-17
+ * */
 public class Variable
 {
+	/**
+	 * Type de la variable courante
+	 * */
 	private TypeVariable type;
-	//private String nom;
+	
+	/**
+	 * Valeur de la variable courante
+	 * */
 	private String valeur;
 	
+	/**
+	 * Permet de créer une variable avec un type et une valeur
+	 * @param type un TypeVariable
+	 * @param valeur un String
+	 * */
 	public Variable(TypeVariable type, String valeur)
 	{
 		this.type = type;
-		//his.nom = nom;
 		this.valeur = valeur;
 	}
-	
-	
-	/*public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}*/
 
 	@Override
 	public String toString()
@@ -28,28 +34,36 @@ public class Variable
 		return this.valeur.toString();
 	}
 	
-	
-	
+	/**
+	 * Retourne le type de la Variable
+	 * @return un TypeVariable
+	 * */
 	public TypeVariable getType() {
 		return type;
 	}
-
+	
+	/**
+	 * Permet de définir le type de la variable
+	 * @param type TypeVariable
+	 * */
 	public void setType(TypeVariable type) {
 		this.type = type;
 	}
-
+	
+	/**
+	 * Retourne la valeur du type
+	 * @return un String
+	 * */
 	public String getValeur() {
 		return valeur;
 	}
-
+	
+	/**
+	 * Permet de définir la valeur de la variable
+	 * @param valeur String
+	 * */
 	public void setValeur(String valeur) {
 		this.valeur = valeur;
-	}
-
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
-
 	}
 
 }
